e61a9df3bb854313799239efc9efe9a35773657e
Dankos <58398839+Danko998@users.noreply.github.com>
2021-03-28 19:32:41 +0300

    4 Lab

--- 3/scr/3.py
+++ 3/scr/3.py
@@ -1,1 +1,8 @@
 #3. Арифметика для разных типов
+print('Введите два целых числа')
+a = int(input())
+b = int(input())
+print('a+b= ', a+b)
+print('a-b= ', a-b)
+print('a*b= ', a*b)
+print('a/b= ', a/b)
---
+++ 4/scr/4.py
@@ -0,0 +1,10 @@
+#4. Обмен значениями
+print('Введите два целых числа')
+a = int(input())
+b = int(input())
+tmp=a
+a=b
+b=tmp
+print('a=',a, ', b=', b)
+a, b = b, a
+print('a=',a, ', b=', b)
