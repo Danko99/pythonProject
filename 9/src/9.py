8cada06556b40fee6abe8e2ca5e53f2720b2358e
Danko <dxarev@gmail.com>
2021-03-29 16:05:26 +0300

    labs

--- 9/src/9.py
+++ 9/src/9.py
@@ -3,11 +3,16 @@ from datetime import datetime
 
 time = input()
 time2 = input()
+
 datetime_object = datetime.strptime(f'{time}', '%I:%M')
 datetime_object2 = datetime.strptime(f'{time2}', '%I:%M')
-result = datetime_object2 - datetime_object
+if datetime_object2<datetime_object:
+    print('Неверное время')
+    exit()
 
-if result.seconds/60 > 15 :
+result = (datetime_object2 - datetime_object).seconds/60
+print(result)
+if result > 15:
     print('Встреча не состоится')
 else:
     print('Встреча состоится')
