db249d775c9375798ea9ac801a3d6608faeeb827
Danko <dxarev@gmail.com>
2021-03-29 14:39:16 +0300

    labs

--- 8/src/8.py
+++ 8/src/8.py
@@ -1,6 +1,8 @@
-A = [0, 0, 0]
-
-A[0], A[1], A[2] = map(str, input().split())
+#8. Калькулятор
+A = [str(i) for i in input().split()]
+if len(A)>3:
+    print('Количество значений больше 3')
+    exit()
 
 if A[1] == '+':
     print(int(A[0], 10) + int(A[2], 10))
