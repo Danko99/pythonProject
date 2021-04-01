23bf96dc1c1c7adaf57538a856fe3af5e1cac1e4
Dankos <58398839+Danko998@users.noreply.github.com>
2021-04-01 22:48:18 +0300

    labs

---
+++ 16/src/16.py
@@ -0,0 +1,16 @@
+#16. Хобби
+import re
+n = int(input())
+A = input().split()
+
+if len(A) != n:
+    print('Количество билетов не совпадает с указанными билетами')
+
+regex = re.compile(r"(^a...55661)")
+list_of_string = [s for s in A if regex.match(s)]
+
+if len(list_of_string)!=0:
+    print(list_of_string)
+else:
+    print(-1)
+
