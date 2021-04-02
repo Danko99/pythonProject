803a1e78338c6aa72330e1243e4d99a5a4bb3d5a
Danko <dxarev@gmail.com>
2021-04-02 18:55:01 +0300

    labs

--- 19/src/19.py
+++ 19/src/19.py
@@ -1,11 +1,11 @@
 # 19. Медвежья память
 
 import itertools
-from itertools import chain
 combinations = []
 quantity = int(input())
 word = input()
 stuff = list(word)
+
 if len(word) != quantity:
     i = 0
     while i < quantity - len(word):
@@ -16,5 +16,5 @@ for L in range(len(stuff), len(stuff) + 1):
     for subset in itertools.permutations(stuff, L):
         combinations.append(subset)
 
-#print(*(i for j in combinations for i in j))
-#print (''.join([str(i) if isinstance(i,int) else i for tup in combinations for i in tup]))
+for i in combinations:
+    print(*i, end=' ', sep='')
