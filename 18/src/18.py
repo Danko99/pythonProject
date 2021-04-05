157d4608880eb7055efb41497be36d068f592080
Danko <dxarev@gmail.com>
2021-04-05 14:31:07 +0300

    labs

--- 18/src/18.py
+++ 18/src/18.py
@@ -1,12 +1,21 @@
 #18. Евротур
-import itertools
-combinations=[]
-word = input()
-stuff = list(word)
-for L in range(len(stuff), len(stuff)+1):
-    for subset in itertools.permutations(stuff, L):
-        combinations.append(subset)
-print(combinations)
+word='halloklempnerdasistfantastischfluggegecheimen'
+words=list(word)
+words_dict={}
+for i in words:
+    words_dict[i] = words.count(i) / len(words)
+probability=[]
+stopWord=input()
+sum=1
+for i in stopWord:
+    if words_dict.keys().__contains__(i):
+        sum*=words_dict[i]
+    else:
+        sum=0
+
+print(sum)
+
+
 
 
 
