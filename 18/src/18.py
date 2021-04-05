b9d0e3dad389440c84d3372f0b85ee9e01efded9
Danko <dxarev@gmail.com>
2021-04-05 14:55:14 +0300

    labs

--- 18/src/18.py
+++ 18/src/18.py
@@ -1,21 +1,16 @@
-#18. Евротур
-word='halloklempnerdasistfantastischfluggegecheimen'
-words=list(word)
-words_dict={}
+# 18. Евротур
+word = 'halloklempnerdasistfantastischfluggegecheimen'
+words = list(word)
+words_dict = {}
 for i in words:
     words_dict[i] = words.count(i) / len(words)
-probability=[]
-stopWord=input()
-sum=1
+probability = []
+stopWord = input()
+sum = 1
 for i in stopWord:
     if words_dict.keys().__contains__(i):
-        sum*=words_dict[i]
+        sum *= words_dict[i]
     else:
-        sum=0
+        sum = 0
 
 print(sum)
-
-
-
-
-
