c7a9e173fe997bbbe913277771182a3f9e832565
Dankos <58398839+Danko998@users.noreply.github.com>
2021-04-05 17:55:00 +0300

    labs

--- 19/src/19.py
+++ 19/src/19.py
@@ -1,20 +1,35 @@
 # 19. Медвежья память
 
 import itertools
+def check(arr):
+    i=0
+    while i<len(arr)-1:
+
+        if arr[i]==arr[i+1]:
+            return True
+        else:
+            return False
+    i+=1
+
 combinations = []
 quantity = int(input())
 word = input()
 stuff = list(word)
 
 if len(word) != quantity:
-    i = 0
-    while i < quantity - len(word):
-        stuff.append(word[i])
-        i += 1
+    combinations = itertools.product(word,repeat=quantity)
+else:
+    for L in range(len(stuff), len(stuff) + 1):
+        for subset in itertools.permutations(stuff, L):
+            combinations.append(subset)
 
-for L in range(len(stuff), len(stuff) + 1):
-    for subset in itertools.permutations(stuff, L):
-        combinations.append(subset)
+i=0
+combinations=list(combinations)
+while i<len(combinations):
+    if check(combinations[i]):
+        combinations.remove(combinations[i])
+    i += 1
+
 
 for i in combinations:
     print(*i, end=' ', sep='')
