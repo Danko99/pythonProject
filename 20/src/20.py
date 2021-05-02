95ba5ee3a474974198b20ae8ce7d91d4ab038567
Dankos <58398839+Danko998@users.noreply.github.com>
2021-05-02 20:17:51 +0300

    labs

--- 20/src/20.py
+++ 20/src/20.py
@@ -1,29 +1,45 @@
+money = int(input())
+
+
 class Alchohol:
     name = ''
-    price = 1
-    v = 1
-    final_price=0
-    final_v=0
+    price = 0
+    v = 0
+    final_price = price
+    final_v = v
+    numb_of_bottle=0
+    last_money=0
 
     def check_price(self):
+        z = self.price
+        while z < money:
+            self.final_price += self.price
+            self.final_v+=self.v
+            self.numb_of_bottle+=1
+            z += self.price
+        self.last_money=money-self.final_price
 
-
-
-
-
-        print(self.name)
 
     def display_info(self):
-        print(self.name)
+        print(self.name, self.numb_of_bottle)
+        print(self.final_v)
+        print(self.last_money)
 
 
-money = int(input())
 k = int(input())
-alco = [Alchohol()]
+
+alco = []
+for i in range(k):
+    alco.append(Alchohol())
+
 i = 0
 
 while i < k:
     alco[i].name, alco[i].price, alco[i].v = input().split()
+    alco[i].price=int(alco[i].price)
+    alco[i].v = int(alco[i].v)
+    alco[i].check_price()
     i += 1
+
 
 alco[0].display_info()
