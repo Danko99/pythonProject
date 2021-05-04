9c7d1b91e0184156f33229c0d1d97888a6c584a5
Dankos <58398839+Danko998@users.noreply.github.com>
2021-05-04 11:52:11 +0300

    labs

---
+++ 28/src/28.py
@@ -0,0 +1,25 @@
+#28. Факторизация натурального числа
+from collections import Counter
+n = int(input("Integer: "))
+factors = []
+d = 2
+m = n
+while d * d <= n:
+        if n % d == 0:
+            factors.append(d)
+            n//=d
+        else:
+            d += 1
+factors.append(n)
+f_factors = Counter(factors)
+str = str(f_factors)
+str = str.replace("Counter(", "", len(f_factors))
+str = str.replace(")", "", len(f_factors))
+str = str.replace("{", "", len(f_factors))
+str = str.replace("}", "", len(f_factors))
+str = str.replace(": 1", "", len(f_factors))
+str = str.replace(", ", "*", len(f_factors))
+str = str.replace(": ", "^", len(f_factors))
+print(str)
+
+
