024885c1dbb3c0009922355ce8e5a1a05c5fc0f8
Danko <dxarev@gmail.com>
2021-07-26 13:03:18 +0300

    Update Distance_between_two_points_Class.py

--- Distance_between_two_points_Class.py
+++ Distance_between_two_points_Class.py
@@ -1,17 +1,18 @@
-import string
 import math
 
 
 class Point:
 
     def set_coordinates(self, *args):
-        alphabet = list(string.ascii_lowercase)
-        if 26 >= len(args) >= 2:
+        if len(args) >= 2:
             for i in range(0, len(args)):
-                setattr(self, alphabet[i], args[i])
+                setattr(self, str(i), args[i])
+        else:
+            print("Переданы неверные координаты")
 
     def get_distance(self, object):
-        if isinstance(object, Point):
+
+        if isinstance(object, Point) and len(self.__dict__.values()) == len(object.__dict__.values()):
             dist_array1 = [int(i) for i in self.__dict__.values()]
             dist_array2 = [int(i) for i in object.__dict__.values()]
             dist = 0
@@ -25,6 +26,6 @@ class Point:
 p1 = Point()
 p2 = Point()
 p1.set_coordinates(1, 2, 8)
-p2.set_coordinates(4, 6, 2)
+p2.set_coordinates(4, 6, 9)
 print(p2.get_distance(p1))
 
