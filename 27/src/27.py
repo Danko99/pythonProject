487f9c30ad44bef164eefe1072162f80f8e7c265
Danko <dxarev@gmail.com>
2021-08-19 13:45:12 +0300

    Update 27.py

--- 27/src/27.py
+++ 27/src/27.py
@@ -1,22 +1,17 @@
 #27. TOP — 5 минимальных
-n = int(input())
-n_= map(int, input().split())
-array=list(n_)
-temp_array=[]
-i=0
-while i < len(array):
-    j=0
-    temp_array.append(array[i])
-    temp_array = sorted(temp_array, reverse=True)
-    while j<len(temp_array):
-        if len(temp_array) > 5:
-            temp_array.remove(temp_array[j])
-        j+=1
-    i+=1
-    print(*temp_array, sep=" ")
-
-
-
+z = int(input())
+listz = [int(i) for i in input().split()]
+temp_list = []
+for i in listz:
+    temp_list.sort(reverse=True)
+    if len(temp_list) == 5:
+        if i < min(temp_list):
+            temp_list.pop(0)
+            temp_list.append(i)
+        print(*temp_list)
+    else:
+        temp_list.append(i)
+        print(*temp_list)
 
 
 
